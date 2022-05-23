78cfb68ee9bafd4d7628c57a2c99b308d7702b99
Abdullah Alam <abdullahalam@protonmail.com>
2022-05-23 10:56:45 +0530

    improve metadata fetching

--- src/components/Photograph.tsx
+++ src/components/Photograph.tsx
@@ -1,28 +1,55 @@
-import { getIpfsURL } from "../helpers";
-import '../styles/Photograph.scss'
+import { useEffect, useState } from 'react';
+import { getIpfsURL } from '../helpers';
+import '../styles/Photograph.scss';
 
 interface PhotographProps {
   photograph: any;
 }
 
 export default function ({ photograph }: PhotographProps) {
+  const [metadata, setMetadata] = useState<any>({});
+
+  useEffect(() => {
+    fetch(getIpfsURL(photograph.photograph_metadata_cid))
+      .then((data: any) => data.json())
+      .then((metadata: any) => {
+        setMetadata(metadata);
+      })
+  }, []);
+
+  const getKeyTitle = (key: string) => {
+    return {
+      title: 'Title',
+      description: 'Description',
+      location: 'Location',
+      tags: 'Tags',
+      photographer: 'Photographer',
+      account: 'Account',
+    }[key]
+  }
+
   return (
     <div className="Photograph">
       <img src={getIpfsURL(photograph.image_cid)}></img>
       <div className="info-section">
         <h3>Info:</h3>
-        <p>
-          <b>Photographer:</b> Abdullah Alam
-        </p>
-        <p>
-          <b>Location:</b> Himachal
-        </p>
-        <p>
-          <b>Tags:</b> landscape, nature, outdoor
-        </p>
+
+        {Object.keys(metadata)
+          .filter((key: string) => metadata[key] !== '')
+          .map((key: string) => {
+            return (
+              <p>
+                <b>{getKeyTitle(key)}:</b> {metadata[key]}
+              </p>
+            );
+          })}
         <div className="buttons">
-          <a className="link-button" href={getIpfsURL(photograph.image_cid)}>Image File</a>
-          <a className="link-button" href={getIpfsURL(photograph.photograph_metadata_cid)}>Metadata</a>
+          <a className="link-button" href={getIpfsURL(photograph.image_cid)}>
+            Image File
+          </a>
+          <a className="link-button" href={getIpfsURL(photograph.photograph_metadata_cid)}>
+            Metadata
+          </a>
         </div>
       </div>
     </div>
